f290c2cd6b7e16a7143f86802e3125d2d451ec42
Ramazan <ramazantur47@gmail.com>
2020-06-22 01:40:07 +0200

    second commit from local repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,7 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("first commit from local repository");
 
 	}
 
